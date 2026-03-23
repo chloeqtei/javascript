2f461edaaed1b07ef97236d3761d412703a6cf1a
chloeqtei <chloeqtei@gmail.com>
2026-03-23 14:57:18 +0700

    javascript

---
+++ bienjs/main.js
@@ -0,0 +1,11 @@
+console.log ("video 17");
+var name = " hoidanit";
+
+//let có thể gangs lại giá trị tuỳ thích
+let name1 = "Le ngoc xuan quynh";
+let age = 30;
+age = 20;
+// const không thể gáng lại giá trị của của hằng số
+const myAge = 20;
+// myAge = 30;
+
---
+++ bienjs/thuchanh.js
@@ -0,0 +1,16 @@
+console.log("video 18");
+
+//number
+const myAge = 18;
+const myScore = 9.1;
+
+//string
+const name = "le ngoc xuan quynh";
+const name1 = `javascript`;
+
+console.log(name);
+
+//boolean: true/false
+const isCheck = true;
+console.log(isCheck);
+
